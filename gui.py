4647b07612cb0838064074def95315d1da7e56b0
TibMont <mirko.fischer.98@web.de>
2020-09-02 19:36:45 +0200

    Node fixed

--- gui.py
+++ gui.py
@@ -10,6 +10,7 @@ class Application(Frame):
     ftree = None
     textbar = None
     lastselected = None
+    opennodes = []
 
     def __init__(self, t, g, master=None):
         super().__init__(master)
@@ -32,11 +33,13 @@ class Application(Frame):
         self.textbar.delete(1.0, END)
         self.textbar.insert(END, r)
         self.tree.item(currentselected, open=TRUE)
-        col = self.tree.identify_column(e.x)
-        if col == 0:
+        if currentselected in self.opennodes:
             self.tree.item(currentselected, open=FALSE)
+            self.opennodes.remove(currentselected)
+        else:
+            self.opennodes.append(currentselected)
         self.lastselected = currentselected
-
+        
     def create_widgets(self):
         scrollbar = Scrollbar(self)
         scrollbar.pack(side=LEFT, fill=Y)
